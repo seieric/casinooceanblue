448dbadd7117c77593ae940523b903f483b0b2af
seieric <30688855+seieric@users.noreply.github.com>
2020-09-13 20:45:50 +0900

    enable ssl redirect in index page

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(forceSsl);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(forceSsl);
 
 app.use('/', indexRouter);
 app.use('/api/', apiRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 module.exports = router;
+
+router.get('/', (req,res) => {
+    res.sendFile(__dirname + '/public/default.html');
+});
